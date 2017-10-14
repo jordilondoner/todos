6484bcc1ff86c05928eefdbcb53e490ac742f7e0
Jordi Mundó <jordimundo@gmail.com>
2017-10-14 13:23:14 +0100

    -Add the ability to remove items. Add an input button to on every todo item to allow the user to remove todos

--- src/components/ItemsList/index.js
+++ src/components/ItemsList/index.js
@@ -3,13 +3,21 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import './styles.css';
 
-export const ItemsList = ({ items }) => {
+export const ItemsList = ({ items, onRemove }) => {
   return (
     <div>
       <ul className={'itemsList-ul'}>
         {items.length < 1 && <p id={'items-missing'}>Add some tasks above.</p>}
         {items.map(item => <li key={item.id}>
           {item.content}
+          <input
+            className={'itemCreator-button'}
+            type="button"
+            value={'Remove task'}
+            onClick={() => {
+              console.log(item.id)
+            }}
+          />
         </li>)}
       </ul>
     </div>
@@ -17,7 +25,7 @@ export const ItemsList = ({ items }) => {
 };
 
 ItemsList.propTypes = {
-  items: PropTypes.array.isRequired,
+  items: PropTypes.array.isRequired
 };
 
 const mapStateToProps = state => {
